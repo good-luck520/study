4a8bfd666cea1b0fb72785823246f47b31cbdb29
cqw <995403055@qq.com>
2020-04-19 18:49:42 +0800

    在heaven分支新建了一个home组件

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data:{
+      msg: "heaven分支的数据"
+  }
+}
+</script>
+<style scoped>
+
+</style>
